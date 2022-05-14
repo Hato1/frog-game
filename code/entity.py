81075e8024368e190f367c174fc0e77aeb16e5c7
Jacob Rogers <jacob.p.r@hotmail.com>
2022-05-14 13:02:09 +1200

    Skeleton code

---
+++ code/entity.py
@@ -0,0 +1,20 @@
+"""Module for animate objects"""
+
+
+class Entity():
+    def __init__(self) -> None:
+        # sprite
+        # orientation [1-4]
+        # blocking?
+        pass
+
+
+class Creature(Entity):
+    def __init__(self) -> None:
+        # state
+        # super init
+        pass
+
+    def update(self) -> tuple:
+        # get position object wants to move to
+        pass
