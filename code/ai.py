c19425c84d220741f5242ffc75a532bba139af73
Jacob Rogers <jacob.p.r@hotmail.com>
2022-05-15 18:23:18 +1200

    refactoring

--- code/ai.py
+++ code/ai.py
@@ -1,78 +1,80 @@
-from helper import add_tuple
-UP, LEFT, DOWN, RIGHT = (-1, 0), (0, -1), (1, 0), (0, 1)
+from helper import UP, LEFT, DOWN, RIGHT
+
 
 class Ai():
     def __init__(self, state: int = 0):
         self.state: int = state
 
-    def get_move(self, position: tuple, map: list) -> tuple:
+    def _get_move(self, position: tuple, _map: list) -> tuple[tuple, int]:
+        """Get the next position object wants to move in, and the resulting state"""
         raise NotImplementedError
+
+    def make_move(self, position: tuple, _map: list) -> tuple:
+        """Get next position and update state"""
+        move, newstate = self._get_move(position, _map)
+        self.state = newstate
+        return move
 
 
 class DeadDoug(Ai):
     """Dead dougs don't die"""
-    def get_move(self, position: tuple, map: list) -> tuple:
-        return position
+    def _get_move(self, position: tuple, map: list) -> tuple[tuple, int]:
+        return position, 0
 
 
 class NormalNorman(Ai):
     """Move right and left (0 or 1) or up and down (2 or 3)"""
-    def get_move(self, position: tuple, map: list) -> tuple:
-        if self.state == 0:
-            self.state = 1
-            return add_tuple(position, RIGHT)
-        elif self.state == 1:
-            self.state = 0
-            return add_tuple(position, LEFT)
-        elif self.state == 2:
-            self.state = 3
-            return add_tuple(position, UP)
-        else:
-            self.state = 2
-            return add_tuple(position, DOWN)
+    def _get_move(self, position: tuple, map: list) -> tuple[tuple, int]:
+        match self.state:
+            case 0:
+                return position + RIGHT, 1
+            case 1:
+                return position + LEFT, 0
+            case 2:
+                return position + UP, 3
+            case 3:
+                return position + DOWN, 2
+        assert False
+
 
 class SpiralingStacy(Ai):
     """Moves anticlockwise (0 is bottom left) or clockwise (4 is bottom left)"""
-    def get_move(self, position: tuple, map: list) -> tuple:
-        if self.state == 0:
-            self.state = 1
-            return add_tuple(position, RIGHT)
-        elif self.state == 1:
-            self.state = 2
-            return add_tuple(position, UP)
-        elif self.state == 2:
-            self.state = 3
-            return add_tuple(position, LEFT)
-        elif self.state == 3:
-            self.state = 0
-            return add_tuple(position, DOWN)
-        elif self.state == 4:
-            self.state = 5
-            return add_tuple(position, UP)
-        elif self.state == 5:
-            self.state = 6
-            return add_tuple(position, RIGHT)
-        elif self.state == 6:
-            self.state = 7
-            return add_tuple(position, DOWN)
-        else: # self.state == 7:
-            self.state = 4
-            return add_tuple(position, LEFT)
+    def _get_move(self, position: tuple, map: list) -> tuple[tuple, int]:
+        match self.state:
+            case 0:
+                return position + RIGHT, 1
+            case 1:
+                return position + UP, 2
+            case 2:
+                return position + LEFT, 3
+            case 3:
+                return position + DOWN, 0
+            case 4:
+                return position + UP, 5
+            case 5:
+                return position + RIGHT, 6
+            case 6:
+                return position + DOWN, 7
+            case 7:
+                return position + LEFT, 4
+        assert False
 
 
 class BarrelingBarrel(Ai):
     """Moves not at all, up/down/left/right in state (0/1/2/3/4+) untill stat"""
-    def get_move(self, position: tuple, map: list) -> tuple:
-        if self.state == 0:
-            return position
-        elif self.state == 1:
-            return add_tuple(position, UP)
-        elif self.state == 2:
-            return add_tuple(position, DOWN)
-        elif self.state == 3:
-            return add_tuple(position, LEFT)
-        else: # self.state == 4:
-            return add_tuple(position, RIGHT)
+    def _get_move(self, position: tuple, map: list) -> tuple[tuple, int]:
+        match self.state:
+            case 0:
+                return position, 0
+            case 1:
+                return position + UP, 1
+            case 2:
+                return position + DOWN, 2
+            case 3:
+                return position + LEFT, 3
+            case 4:
+                return position + RIGHT, 4
+        assert False
 
 
 class DirtyDan(Ai):
--- code/helper.py
+++ code/helper.py
@@ -1,9 +1,21 @@
 """Various helper functions"""
+from __future__ import annotations
 import pygame
 
 
 def add_tuple(x: tuple, y: tuple) -> tuple:
     return tuple(x + y for x, y in zip(x, y))
+
+
+class Vector(tuple):
+    def __add__(self, thing2: tuple) -> Vector:
+        return Vector((self[0]+thing2[0], self[1]+thing2[1]))
+
+    def __type__(self) -> type[tuple]:
+        return tuple
+
+
+UP, LEFT, DOWN, RIGHT = Vector((-1, 0)), Vector((0, -1)), Vector((1, 0)), Vector((0, 1))
 
 
 assets = {
