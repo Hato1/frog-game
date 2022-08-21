0c17ea3708385dd146fb193d8d89bc0aa0373035
LB <LB@ded.link>
2022-08-21 14:17:05 +1200

    Road mapped conflict resolution function calling

--- scripts/collision_resolver.py
+++ scripts/collision_resolver.py
@@ -1,5 +1,16 @@
 """Behaviours for collisions between each pair of entities"""
 import logging
+#from .entity import AI_DICT
+
+# Current AIs: AI_DICT = {
+#    "NormalNorman": NormalNorman,
+#    "DeadDoug": DeadDoug,
+#    "SpiralingStacy": SpiralingStacy,
+#    "BarrelingBarrel": BarrelingBarrel,
+#    "TrickyTrent": TrickyTrent,
+#     PLAYER UGH
+#}
+
 
 
 def collision_resolver(
@@ -8,10 +19,34 @@ def collision_resolver(
 		collision_pos: tuple,
 		new_map: "Map",
 		collision_locs: list
-) -> None:
-	logging.debug(msg="Ouch!")
+	) -> None:
+	""" This fn gets the entities, abd calls the appropriate fn based on their strategy"""
+	# CAUTION, I THINK THIS MIGHT DUPLIACATE ENTITIES NOT GET POINTERS JACOB???!?
 	entity_pair = [new_map[collision_pos][a] for a in current_collision]
-	for entity in entity_pair:
-		if str(entity) == "Player":
-			entity.alive = False
+	logging.debug(entity_pair)
+	entity_pair.sort(key = lambda ent: ent.get_strategy_name())
+	entity_strategies = [ent.get_strategy_name() for ent in entity_pair]
+	try:
+		temp_fn = locals()["_".join(entity_strategies)]
+	except:
+		temp_fn = no_conflict
+	temp_fn(current_collision, remaining_pairs, collision_pos, new_map, collision_locs)
+
+	return
+
+def no_conflict(
+		current_collision: tuple,
+		remaining_pairs: list,
+		collision_pos: tuple,
+		new_map: "Map",
+		collision_locs: list) -> None:
+	return
+
+def NormalNorman_NormalNorman(
+		current_collision: tuple,
+		remaining_pairs: list,
+		collision_pos: tuple,
+		new_map: "Map",
+		collision_locs: list) -> None:
+	# LEAPFROG #Prefreence to up, followed by right
 	return
