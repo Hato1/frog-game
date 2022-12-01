5dbf69e0c67a1420ce001e09443e7ec68b2b1916
shinminmetroskyline <kieran.eadonjones@gmail.com>
2022-12-01 14:49:46 +1300

    Beginnings of Entity parser
    currently does no take into account AI, as Ais are in.a .json file.

--- gui/map_parser.py
+++ gui/map_parser.py
@@ -1,12 +1,15 @@
 """Module for handling parsing of files created with the world builder for game use"""
+import pickle
 from pathlib import Path
 
 import pygame as pg
 
 from game.entity import Entity
+from game.helper import Point
 from GAME_CONSTANTS import *
 
 world_dir = Path("maps")
+assert world_dir.exists(), "Wrong CWD!"
 
 # Implementation notes for Kiran:
 # Currently we use gui.make_basemap to load background from the old map format.
@@ -16,13 +19,29 @@ world_dir = Path("maps")
 # Finally, implement the below two functions
 # The new map format is new, you will encounter issues and find things that could be more efficient.
 # Note these things down for later review.
-# -J
+# - Jacob
 
 
-def load_entities(world: str | Path) -> list[Entity]:
-    world = world_dir / world
-    assert world.exists()
-    return []
+def load_entities(world_name: str | Path) -> list[Entity]:
+    world_file = world_dir / world_name
+    assert world_file.exists()
+
+    with open(world_file, "rb") as file:
+        world = pickle.load(file)
+
+    all_entities_map = world[1]
+    all_entities_list = []
+
+    for x, column in enumerate(all_entities_map):
+        for y, tile in enumerate(column):
+            for entity in tile:
+                # InvisWall not currently supported
+                if entity["name"] == "InvisWall":
+                    continue
+                new_entity_obj = Entity(entity["name"], position=Point(x, y))
+                all_entities_list.append(new_entity_obj)
+
+    return all_entities_list
 
 
 def load_background(world: str | Path) -> pg.Surface:
@@ -39,3 +58,6 @@ def load_background(world: str | Path) -> pg.Surface:
     bg = pg.Surface((padded_width * TSIZE, padded_height * TSIZE))
 
     return bg
+
+
+load_entities("map1.map")
